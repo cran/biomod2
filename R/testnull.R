7dd3d40e13eb6ef758c12123c7b6dd55d427187d
Damien Georges <damien.georges2@gmail.com>
2012-09-14 00:00:00 +0000

    version 1.3.5

--- R/testnull.R
+++ R/testnull.R
@@ -1,11 +1,17 @@
 .testnull <-
 function(object, Prev = 0.5 , dat){
-
-    if(object$deviance == object$null.deviance){
-        if(Prev < 0.5) pred <- rep(0, nrow(dat))
-        if(Prev >= 0.5) pred <- rep(1, nrow(dat))
+  
+  if( is.finite(object$deviance) & is.finite(object$null.deviance)){
+    if(object$deviance != object$null.deviance){
+      pred <- predict(object, dat, type="response")
     }
-    else pred <- predict(object, dat, type="response")    
+  }
+  
+  if(!exists('pred')){
+    if(Prev < 0.5) pred <- rep(0, nrow(dat))
+    if(Prev >= 0.5) pred <- rep(1, nrow(dat))
+  }
+
     return(pred)
 }
 
